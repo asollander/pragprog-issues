01e969d0847f7640789d5ebc841a1b8391263481
Anders Sollander <asolland@mathworks.com>
2023-07-09 18:16:48 +0200

    Prepare for documentation generation

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/asollander/pragprog-issues",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -29,7 +31,11 @@ defmodule Issues.MixProject do
     [
       {:httpoison, "~> 2.0"},
       # Book recommends poison, but Jason may be good alternative
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      # For documentation
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      # And an output formatter
+      { :earmark, "~> 1.4.0" },
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
